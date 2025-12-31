fac64f5ea8c41631354764c54211d3f5fa8cdf4b
Elias Nicolas <en9552627@gmail.com>
2025-12-30 19:27:44 -0800

    Update 4DOF_Script.ino

--- 4DOF_Script/4DOF_Script.ino
+++ 4DOF_Script/4DOF_Script.ino
@@ -1,19 +1,26 @@
 /*
  */
 #include <math.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver arm = Adafruit_PWMServoDriver();
+
+#define servoMIN 150
+#define servoMAX 600
 #define pi 3.1415926535897932384626433832795
 
-const int base_servo = 1;
-const int servo_1 = 2;
-const int servo_2 = 3;
-const int servo_3 = 4;
+const float final_x = 89;
+const float final_y = 89;
+
+const int base_servo = 0;
+const int servo_1 = 1;
+const int servo_2 = 2;
+const int servo_3 = 3;
 
 //Units in mm
 const float link_one_length = 65;
 const float link_two_length = 65;
 const float link_three_length = 10;  
-const float final_x = 89;
-const float final_y = 89;
 
 float r2d(float x) {
     float ans;
@@ -64,9 +71,22 @@ float theta_base(){
   return theta_base;
 }
 
+float calibrate(float x){
+  float y = x + 90;
+  return y;
+}
+
+float d2P(float x){  
+  int pulse = map(x,0, 180, SERVOMIN,SERVOMAX);   
+  return pulse;
+}
+
 void setup() {
   //Arduino Setup
   Serial.begin(9600);
+  arm.begin();
+  arm.setPWMFreq(60);
+
   pinMode(base_servo, OUTPUT);
   pinMode(servo_1, OUTPUT);
   pinMode(servo_2, OUTPUT);
@@ -82,8 +102,20 @@ void loop() {
     String msg = Serial.readString();
   }
 
+  //Call all Calculations
   float t1 = theta1();
   float t2 = theta2();
   float t3 = theta3(t1, t2);
   float tb = theta_base();
+
+  //Calibrate them taking into account the 90 degree offset
+  t1 = calibrate(t1);
+  t2 = calibrate(t2);
+  t3 = calibrate(t3);
+  tb = calibrate(tb);
+
+  arm.setPWM(1,0,d2p(t1));
+  arm.setPWM(2,0,d2p(t2));
+  arm.setPWM(3,0,d2p(t3));
+  arm.setPWM(0,0,d2p(tb));
 }
